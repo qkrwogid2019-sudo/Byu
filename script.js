0a6538b90c31789bc598ee2ec0821b07d73d249a
qkrwogid2019-sudo <qkrwogid2019@gmail.com>
2025-12-13 09:57:00 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,39 @@
 const loveFaces = [
   "img/love_01.png",
   "img/love_02.png",
-  "img/love_03.png"
+  "img/love_03.png",
   "img/love_04.png"
 ];
 
 const angryFaces = [
   "img/angry_01.png",
-  "img/angry_02.png"
-  "img/angry_03.png"
+  "img/angry_02.png",
+  "img/angry_03.png",
   "img/angry_04.png"
-
 ];
 
+let liking = 0;
 const face = document.getElementById("face");
+const gauge = document.getElementById("likeGauge");
 
 function randomFrom(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
-function updateFace() {
-  // 좋아할수록 화남 (반동형성)
-  const liking = Math.random();
+function updateGauge() {
+  gauge.style.width = `${liking * 100}%`;
+}
+function flashFaces(faceList, duration = 500, finalFace) {
+  const interval = 80;
+  let elapsed = 0;
 
-  if (liking > 0.4) {
-    face.src = randomFrom(angryFaces);
-  } else {
-    face.src = randomFrom(loveFaces);
-  }
+  const flicker = setInterval(() => {
+    face.src = randomFrom(faceList);
+    elapsed += interval;
+
+    if (elapsed >= duration) {
+      clearInterval(flicker);
+      face.src = finalFace;
+    }
+  }, interval);
 }
