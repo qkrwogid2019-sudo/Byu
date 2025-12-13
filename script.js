59f06c7e3906191218d49857d273484f0acf424d
qkrwogid2019-sudo <qkrwogid2019@gmail.com>
2025-12-13 09:46:18 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+const loveFaces = [
+  "img/love_01.png",
+  "img/love_02.png",
+  "img/love_03.png"
+  "img/love_04.png"
+];
 
+const angryFaces = [
+  "img/angry_01.png",
+  "img/angry_02.png"
+  "img/angry_03.png"
+  "img/angry_04.png"
+
+];
+
+const face = document.getElementById("face");
+
+function randomFrom(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function updateFace() {
+  // 좋아할수록 화남 (반동형성)
+  const liking = Math.random();
+
+  if (liking > 0.4) {
+    face.src = randomFrom(angryFaces);
+  } else {
+    face.src = randomFrom(loveFaces);
+  }
+}
